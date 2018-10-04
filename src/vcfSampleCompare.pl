e53f38a0ec5d8530909ba92cb48ddd2fa41ffaa1
hepcat72 <hepcat72@gmail.com>
2018-10-04 18:54:52 -0400

    Version++.
    Files checked in: src/vcfSampleCompare.pl

--- src/vcfSampleCompare.pl
+++ src/vcfSampleCompare.pl
@@ -16,7 +16,7 @@ use warnings;
 use strict;
 use CommandLineInterface;
 
-our $VERSION = '2.002';
+our $VERSION = '2.003';
 setScriptInfo(VERSION => $VERSION,
               CREATED => '6/22/2017',
               AUTHOR  => 'Robert William Leach',
