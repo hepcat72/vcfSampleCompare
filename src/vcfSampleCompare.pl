46f079473051d2c6b6d2bc50e77c55c642c43350
hepcat72 <hepcat72@gmail.com>
2018-10-05 12:04:36 -0400

    New version to reflect change to test script.
    Files checked in: src/vcfSampleCompare.pl

--- src/vcfSampleCompare.pl
+++ src/vcfSampleCompare.pl
@@ -16,7 +16,7 @@ use warnings;
 use strict;
 use CommandLineInterface;
 
-our $VERSION = '2.003';
+our $VERSION = '2.004';
 setScriptInfo(VERSION => $VERSION,
               CREATED => '6/22/2017',
               AUTHOR  => 'Robert William Leach',
