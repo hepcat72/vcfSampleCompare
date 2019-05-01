7072a54c0114dc26b877d14dbbfbc70fa82a95d8
hepcat72 <hepcat72@gmail.com>
2019-05-01 16:32:41 -0400

    Version increment.
    Files checked in: src/vcfSampleCompare.pl

--- src/vcfSampleCompare.pl
+++ src/vcfSampleCompare.pl
@@ -16,7 +16,7 @@ use warnings;
 use strict;
 use CommandLineInterface;
 
-our $VERSION = '2.010';
+our $VERSION = '2.011';
 setScriptInfo(VERSION => $VERSION,
               CREATED => '6/22/2017',
               AUTHOR  => 'Robert William Leach',
